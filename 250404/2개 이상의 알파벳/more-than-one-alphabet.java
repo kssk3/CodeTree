f4c198a6d8306e0907a1485bd77fb805b3a66f65
Mercury <kss8014@daum.net>
2025-04-04 11:06:44 +0900

    Title: 2개 이상의 알파벳, Time: 112ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250404/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+
+    public static String str;
+    public static int len;
+    public static int count = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        str = sc.next();
+
+        validate(str);
+    }
+
+    private static void validate(String str){
+        len = str.length();
+        for(int i = 0; i < len - 1; i++){
+            if(str.charAt(i) != str.charAt(i + 1)){
+                count++;
+            }
+        }
+
+        if(count > 1){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+
+}
