f1bee80a41a0c6e519bc055bd2e53e6072fa1713
Mercury <kss8014@daum.net>
2025-04-07 12:07:52 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250407/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
+++ 250407/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static int[] arr;
 
-    public static int max;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -20,9 +18,12 @@ public class Main {
     }
 
     public static int findMax(int n){
-        if(n == 0) return arr[0];
+        if(n == 0) return arr[n];
 
-        return Math.max(findMax(n - 1), arr[n]);
+        int prevMax = findMax(n - 1);
+        int curr = arr[n];
+
+        return (prevMax > curr) ? prevMax : curr;
     }
 
 }
