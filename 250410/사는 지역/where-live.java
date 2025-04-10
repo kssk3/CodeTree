a3478fe364484e53c1c9a521dec84cbe3bad48e3
Mercury <kss8014@daum.net>
2025-04-10 11:25:06 +0900

    Title: 사는 지역, Time: 123ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250410/사는 지역/where-live.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Person[] people = new Person[n];
+        for(int i = 0; i < n; i++){
+            String a = sc.next();
+            String b = sc.next();
+            String c = sc.next();
+            people[i] = new Person(a,b,c);
+        }
+
+        int k = ((int)(people[0].name.charAt(0)));
+        for(int i = 1; i < n; i++){
+            if(people[i-1].name.charAt(0) < people[i].name.charAt(0)){
+                k = i;
+            }
+        }
+
+        people[k].InfoPrint();
+    }
+}
+
+class Person{
+    String name, address, location;
+
+    public Person(String name, String address, String location){
+        this.name = name;
+        this.address = address;
+        this.location = location;
+    }
+
+    public void InfoPrint(){
+        System.out.println("name " + name);
+        System.out.println("addr " + address);
+        System.out.println("city " + location);
+    }
+}
