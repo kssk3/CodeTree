48e250023fa8bcba260002513e1efa9654b7c01b
Mercury <kss8014@daum.net>
2025-04-14 11:52:15 +0900

    Title: Time to Time, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250414/Time to Time/time-to-time.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h1 = sc.nextInt();
+        int m1 = sc.nextInt();
+        Time timeA = new Time(h1%10,m1);
+
+        int h2 = sc.nextInt();
+        int m2 = sc.nextInt();
+        Time timeB = new Time(h2%10,m2);
+
+        System.out.println(timeB.sum() - timeA.sum());
+        
+        
+    }
+}
+
+class Time{
+    int hour, min;
+
+    public Time(int hour, int min){
+        this.hour = hour;
+        this.min = min;
+    }
+
+    public int sum(){
+        return (this.hour * 60) + this.min;
+    }
+}
