078451e7399af23dad45a13ce25ff69d7e923633
Mercury <kss8014@daum.net>
2025-04-14 12:08:47 +0900

    Title: Date to Date, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250414/Date to Date/date-to-date.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                                    //   1  2.  3.  4.  5.  6.  7.  8.  9. 10.  11. 12 
+        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int sum1 = d1;
+
+        for(int i = 0; i < m1; i++){
+            sum1 += num_of_days[i];
+        }
+
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int sum2 = d2;
+
+        for(int i = 0; i < m2; i++){
+            sum2 += num_of_days[i];
+        }
+        
+        // System.out.println(sum1);
+        // System.out.println(sum2);
+
+        System.out.println((sum2 - sum1) + 1);
+
+    }
+}
