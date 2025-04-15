84f799df836393cb63171bf880b273ea2f8a9c9b
Mercury <kss8014@daum.net>
2025-04-15 10:57:17 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250415/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int[] DAY_OF_MONTH =
+                        //1,  2, 3,  4,  5. , 6, 7 , 8 , 9 , 10, 11, 12
+            new int []{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int sum1 = sum(m1,d1);
+
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int sum2 = sum(m2,d2);
+
+        if(sum1 > sum2){
+            int number = sum1 - sum2;
+            switch(number%7){
+                case 0 : System.out.println("Mon"); default;
+                case 1 : System.out.println("Tue"); default;
+                case 2 : System.out.println("Wed"); default;
+                case 3 : System.out.println("Thu"); default;
+                case 4 : System.out.println("Fri"); default;
+
+            }
+        }
+    }
+
+    public static int sum(int m, int d){
+        int sum = 0;
+
+        for(int i = 1; i < m; i++){
+            sum += DAY_OF_MONTH[i];
+        }
+        return sum + d;
+    }
+}
