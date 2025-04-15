d9143b69f6aacac2bb7d9ff60276badc97d5a552
Mercury <kss8014@daum.net>
2025-04-15 11:27:14 +0900

    Title: 그 요일은, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250415/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+
+    public static int[] day_of_month =
+         new int[] {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static String[] day =
+         new String[]{"Mon", "Tue", "Wed", "Thu","Fri","Sat","Sun"};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int total = totalDays(m2, d2) - totalDays(m1, d1);
+        String s = sc.next();
+
+        System.out.println(total/7 + 1);
+
+
+    }
+
+    public static int totalDays(int m, int d){
+        int total = 0;
+
+        for(int i = 1; i < m; i++){
+            total += day_of_month[i];
+        }
+
+        return total + d;
+    }
+}
