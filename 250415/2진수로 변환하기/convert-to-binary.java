315e3fd269bda2157b8cda8bfe62e87badf7647d
Mercury <kss8014@daum.net>
2025-04-15 12:40:41 +0900

    Title: 2진수로 변환하기, Time: 114ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250415/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[8];
+
+        int count = 0;
+
+
+        while(true){
+            if(n < 2){arr[count++] = n;
+            break;}
+
+            arr[count++] = n % 2;
+            n /= 2;
+        }
+
+        for(int i = count - 1; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
