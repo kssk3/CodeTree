2e6a123b4c98233defe90c1a1c2ff4a3f0f7465a
Mercury <kss8014@daum.net>
2025-04-16 11:48:49 +0900

    Title: 진수 to 진수, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250416/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        String binary = sc.next();
+        int num = 0;
+
+        for(int i = 0;  i < binary.length(); i++){
+            num = num * a + (binary.charAt(i) - '0');
+        }
+
+        int[] arr = new int[100];
+        int count = 0;
+
+        while(true){
+            if(num < 2){
+                arr[count++] = num;
+                break;
+            }
+            arr[count++] = num % b;
+            num /= b;
+        }
+
+        for(int i = count - 1; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
