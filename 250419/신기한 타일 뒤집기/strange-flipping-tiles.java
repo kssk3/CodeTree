ae9d44075b1b6eeb7778b0452f4ce1f03ad255f7
Mercury <kss8014@daum.net>
2025-04-19 15:55:42 +0900

    Title: 신기한 타일 뒤집기, Time: 193ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 250419/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int MAX = 20_000;
+    public static int start = 10_000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char[] color = new char[MAX + 1];
+        for(int i = 0; i < color.length; i++){
+            color[i] = 'N';
+        }
+
+        for(int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            char c = sc.next().charAt(0);
+
+            if(c == 'R'){
+                for(int j = start; j < start + x; j++){
+                    if(color[j] == 'N' || color[j] == 'W'){
+                        color[j] = 'B';
+                    }
+                }
+                start += x;
+            }
+
+            if(c == 'L'){
+                for(int j = start - 1; j >= start - x; j--){
+                    if(color[j] == 'N' || color[j] == 'B'){
+                        color[j] = 'W';
+                    }
+                }
+                start -= x;
+            }
+        }
+
+
+        int wCount = 0, bCount = 0;
+        for(char c : color){
+            if(c == 'W'){wCount++;}
+            else if(c == 'B'){bCount++;}
+        }
+
+        System.out.println(wCount + " " + bCount);
+    }
+}
