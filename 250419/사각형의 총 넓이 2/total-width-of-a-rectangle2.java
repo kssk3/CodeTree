6ee53830457ab7fc508b8c68c99c654fe7d49022
Mercury <kss8014@daum.net>
2025-04-19 18:16:27 +0900

    Title: 사각형의 총 넓이 2, Time: 132ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 250419/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250419/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,8 +1,34 @@
+import java.util.*;
+
 public class Main {
     
-    public static final int OFFSET = 100;
-    
     public static void main(String[] args) {
-        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[201][201];
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            for(int j = x1; j < x2; j++){
+                for(int k = y1; k < y2; k++){
+                    arr[j][k]++;
+                }
+            }
+        }
+
+        for(int x = 0; x < 201; x++){
+            for(int y = 0; y < 201; y++){
+                if(arr[x][y] > 0){count++;}
+            }
+        }
+
+        System.out.println(count);
     }
 }
