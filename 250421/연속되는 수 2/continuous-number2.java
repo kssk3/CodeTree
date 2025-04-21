e39d92df3fdbfc0d7ad9a90ff95332c3d710b0be
Mercury <kss8014@daum.net>
2025-04-21 12:27:07 +0900

    Title: 연속되는 수 2, Time: 182ms, Memory: 12MB, Status: Passed - Codetree

--- 250421/연속되는 수 2/continuous-number2.java
+++ 250421/연속되는 수 2/continuous-number2.java
@@ -11,11 +11,25 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
+        int maxCount = 0;
         int cnt = 0;
+
         for(int i = 0; i < arr.length; i++){
             if(i == 0 || arr[i] ==  arr[i - 1]){cnt++;}
+            else{
+                if(cnt > maxCount)
+                {
+                    maxCount = cnt;
+                }
+                cnt = 1;
+            }
         }
 
-        System.out.println(cnt);
+        if(maxCount > cnt){
+            System.out.println(maxCount);
+        }else{
+            System.out.println(cnt);
+        }
+
     }
 }
