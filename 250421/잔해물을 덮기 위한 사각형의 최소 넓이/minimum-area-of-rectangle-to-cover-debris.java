0a3cafbf924371a0f26d90bc99946f26337ec22d
Mercury <kss8014@daum.net>
2025-04-21 11:14:02 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 192ms, Memory: 27MB, Status: Passed - Codetree

--- 250421/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250421/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -9,48 +9,51 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[MAX][MAX];
 
-
-        for(int i = 0; i < 1; i++){
+        for(int i = 0; i < 2; i++){
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
             int x2 = sc.nextInt();
             int y2 = sc.nextInt();
 
-            for(int x = x1 + OFFSET; x < x2 + OFFSET; x++){
-                for(int y = y1 + OFFSET; y < y2 + OFFSET; y++){
-                    arr[x][y] = 1;
+            if(i == 0){
+                for(int x = x1 + OFFSET; x < x2 + OFFSET; x++){
+                    for(int y = y1 + OFFSET; y < y2 + OFFSET; y++){
+                        arr[x][y] = 1;
+                    }
+                }
+            }
+            if(i == 1){
+                for(int x = x1 + OFFSET; x < x2 + OFFSET; x++){
+                    for(int y = y1 + OFFSET; y < y2 + OFFSET; y++){
+                        arr[x][y] = 0;
+                    }
                 }
             }
         }
 
-        for(int i = 0; i < 1; i++){
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
+        // 남은 잔해의 경계 찾기
+        int minX = MAX;
+        int maxX = -1;
+        int minY = MAX;
+        int maxY = -1;
+        boolean found = false;
 
-            for(int x = x1 + OFFSET; x < x2 + OFFSET; x++){
-                for(int y = y1 + OFFSET; y < y2 + OFFSET; y++){
-                    arr[x][y] = 0;
+        for(int x = 0; x < MAX; x++){
+            for(int y = 0; y < MAX; y++){
+                if(arr[x][y] == 1){
+                    minX = Math.min(minX, x);
+                    maxX = Math.max(maxX, x);
+                    minY = Math.min(minY, y);
+                    maxY = Math.max(maxY, y);
+                    found = true;
                 }
             }
         }
 
-        int lowMax = 0;
-        int rowMax = 0;
-
-        for(int x = 0; x < arr.length; x++){
-            int low = 0;
-            int row = 0;
-            for(int y = 0; y < arr[x].length; y++){
-                if(arr[y][x] == 1){low++;}
-                if(arr[x][y] == 1){row++;}
-
-                if(low > lowMax){lowMax = low;}
-                if(row > rowMax){rowMax = row;}
-        
-            }
+        if(found){
+            System.out.println((maxX - minX + 1) * (maxY - minY + 1));
+        } else {
+            System.out.println(0);
         }
-        System.out.println(lowMax * rowMax);
     }
 }
