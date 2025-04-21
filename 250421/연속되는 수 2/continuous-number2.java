644852c4c98ad7b33ba8d7b2b7432b4b261f7fee
Mercury <kss8014@daum.net>
2025-04-21 12:01:45 +0900

    Title: 연속되는 수 2, Time: 193ms, Memory: 12MB, Status: Wrong Answer - Codetree

--- 250421/연속되는 수 2/continuous-number2.java
+++ 250421/연속되는 수 2/continuous-number2.java
@@ -13,7 +13,7 @@ public class Main {
 
         int cnt = 0;
         for(int i = 0; i < arr.length; i++){
-            if(i == 0 || arr[i] !=  arr[i - 1]){cnt++;}
+            if(i == 0 || arr[i] ==  arr[i - 1]){cnt++;}
         }
 
         System.out.println(cnt);
