4870417becabb78fd1a98d4f4ea33720688f4973
Mercury <kss8014@daum.net>
2025-04-22 11:23:06 +0900

    Title: 연속되는 수 4, Time: 161ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250422/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 1;
+        int max = 0;
+
+        for(int i = 1; i < n; i++){
+            if(arr[i] > arr[i-1]){count++;}
+            else{max = Math.max(count, max);
+            count = 1;}
+        }
+
+        max = Math.max(count, max);
+
+        System.out.println(max);
+    }
+}
