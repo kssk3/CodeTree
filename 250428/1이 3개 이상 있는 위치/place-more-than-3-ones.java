ba4f6089584936b73fc5d817d7a523409211ff66
Mercury <kss8014@daum.net>
2025-04-28 11:14:55 +0900

    Title: 1이 3개 이상 있는 위치, Time: 196ms, Memory: 13MB, Status: Runtime Error - Codetree

---
+++ 250428/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n;
+
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        n = sc.nextInt();
+        
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int count = 0;
+        int nx,ny;
+
+        for(int x = 0; x < n; x++){
+            for(int y = 0; y < n; y++){
+                int cnt = 0;
+                for(int i = 0; i < n; i++){
+                    nx = x + dx[i];
+                    ny = y + dy[i];
+                    if(inRange(nx,ny,n) && arr[nx][ny] == 1){
+                        cnt++;
+                    }
+                }
+                if(cnt >= 3){count++;}
+            }
+        }
+
+        System.out.println(count);
+    }
+    public static boolean inRange(int x, int y, int n){
+        return (0 <= x && x < n && 0 <= y && y < n);
+    }
+}
