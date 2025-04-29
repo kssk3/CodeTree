1e50460997090c7d238258e9022758f6aa5e98f7
Mercury <kss8014@daum.net>
2025-04-29 11:56:44 +0900

    Title: 되돌아오기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250429/되돌아오기/come-back.java
+++ 250429/되돌아오기/come-back.java
@@ -1,67 +1,48 @@
-import java.util.*;
+    import java.util.*;
 
-public class Main {
+    public class Main {
 
-    public static int[] dx = {0, 1, 0, -1};
-    public static int[] dy = {1, 0, -1, 0};
+        public static int[] dx = {0, 1, 0, -1};
+        public static int[] dy = {1, 0, -1, 0};
 
-    public static int[][] arr = new int[101][101];
+        public static int[][] arr = new int[100][100];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
 
-        int x = 4, y = 4;
-        int position = 1;
-        arr[x][y] = position;
+            int x = 0, y = 0;
+            int time = 0;
 
-        int count = 0;
+            for(int i = 0; i < n; i++){
+                char direction = sc.next().charAt(0);
+                int distance = sc.nextInt();
 
-        boolean result = false;
-        while(n --> 0){
-            char direction = sc.next().charAt(0);
-            int move = sc.nextInt();
+                int dir = getDireciotn(direction);
 
-            int dir = Dig(direction);
+                for(int j = 0; j < distance; j++){
+                    x += dx[dir];
+                    y += dy[dir];
+                    time++;
 
-            for(int i = 0; i < move; i++){
-                int nx = x + dx[dir], ny = y + dy[dir];
-                if(!isRange(nx,ny)){
-                    dir = (dir + 1) % 4; 
+                    if(x == 0 && y == 0){
+                        System.out.println(time);
+                        return;
+                    }
                 }
-
-                if(arr[nx][ny] == 1){
-                    result = true;
-                    count = position;
-                    break;
-                }
-                x = x + dx[dir]; y = y + dy[dir];
-                position++;
-                arr[x][y] = position;
             }
-        }
-
-        if(result){
-            System.out.println(count);
-        }else{
             System.out.println(-1);
         }
 
-    }
-
-    public static boolean isRange(int n, int y){
-        return (0 <= n && n < arr.length && 0 <= y && y < arr[0].length);
-    }
-
-    public static int Dig(char c){
-        if(c == 'E'){
-            return 0;
-        }else if(c == 'S'){
-            return 1;
-        }else if(c == 'W'){
-            return 2;
-        }else{
-            return 3;
+        public static int getDireciotn(char c){
+            if(c == 'E'){
+                return 0;
+            }else if(c == 'S'){
+                return 1;
+            }else if(c == 'W'){
+                return 2;
+            }else{
+                return 3;
+            }
         }
     }
-}
