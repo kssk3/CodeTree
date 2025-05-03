4be9a88b0830e6771f4d463a7e243cab960af7a7
Mercury <kss8014@daum.net>
2025-05-03 14:34:19 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 177ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250503/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+    
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {1, 0, -1, 0};
+    public static int n;
+    public static int t;
+    public static int x;
+    public static int y;
+
+    public static int dir = 3;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        t = sc.nextInt();
+        String s = sc.next();
+
+        int[][] arr = new int[n][n];
+        int count = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                arr[i][j] = count++;
+            }
+        }
+
+        x = n / 2;
+        y = n / 2;
+
+        int sum = arr[x][y];
+        for(int i = 0; i < t; i++){
+            char c = s.charAt(i);
+            if(c == 'R' || c == 'L'){
+                dir = getDirection(c);
+                continue;
+            }
+            int nx = x + dx[dir], ny = y + dy[dir];
+            if(!isRange(nx,ny)){
+                continue;
+            }
+            x += dx[dir];
+            y += dy[dir];
+            sum += arr[x][y];
+        }
+        System.out.println(sum);
+    }
+
+    public static int getDirection(char input){
+        if(input == 'L')return dir = (dir - 1 + 4) % 4;
+        else return dir = (dir + 1) % 4;
+    }
+
+    public static boolean isRange(int x, int y){
+        return (0 <= x && x < n && 0 <= y && y < n);
+    }
+}
