da49499d9e2d6b8e13c499eb178c7c48675299b3
Mercury <kss8014@daum.net>
2025-05-03 14:52:12 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 232ms, Memory: 17MB, Status: Passed - Codetree

--- 250503/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
+++ 250503/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.java
@@ -17,11 +17,13 @@ public class Main {
         t = sc.nextInt();
         String s = sc.next();
 
+        int a = 3;
+        int b = 3;
+
         int[][] arr = new int[n][n];
-        int count = 1;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                arr[i][j] = count++;
+                arr[i][j] = sc.nextInt();
             }
         }
 
