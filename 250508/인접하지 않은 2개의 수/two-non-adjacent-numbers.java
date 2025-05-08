0bfef2facc41ab5c09035e88b36df1512012b6b1
Mercury <kss8014@daum.net>
2025-05-08 11:04:57 +0900

    Title: 인접하지 않은 2개의 수, Time: 148ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250508/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+
+        for(int i = 0; i < n - 2; i++){
+            for(int j = i + 2; j < n; j++){
+                int sum = arr[i] + arr[j];
+                max = Math.max(sum, max);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
