8cedd984da0dc0f5d4c9712a0301af8e82f1ac6f
Mercury <kss8014@daum.net>
2025-05-11 21:37:17 +0900

    Title: 최고의 13위치 2, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250511/최고의 13위치 2/best-place-of-13-2.java
+++ 250511/최고의 13위치 2/best-place-of-13-2.java
@@ -1,18 +1,11 @@
 import java.util.*;
 
 public class Main {
-
-    public static int n;
-
-    public static int[][] arr;
-
-    public static int[][] findCoin = new int[0][2];
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
-        arr = new int[n][n];
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
@@ -20,49 +13,32 @@ public class Main {
             }
         }
 
-
-        int[] findCoin = new int[n];
-
-        if(n < 6){
-            for(int i = 0; i < n; i++){
-                int max = 0;
-                for(int j = 0; j < n - 2; j++){
-                    int count = 0;
-                    for(int k = 0; k < 3; k++){
-                        if(arr[i][j + k] == 1){count++;}
+        int maxCoins = 0;
+        
+        // 모든 가능한 두 개의 1*3 격자 배치를 시도
+        for (int r1 = 0; r1 < n; r1++) {
+            for (int c1 = 0; c1 <= n - 3; c1++) {
+                // 첫 번째 격자의 동전 개수 계산
+                int coins1 = arr[r1][c1] + arr[r1][c1 + 1] + arr[r1][c1 + 2];
+                
+                // 두 번째 격자의 모든 가능한 위치를 시도
+                for (int r2 = 0; r2 < n; r2++) {
+                    for (int c2 = 0; c2 <= n - 3; c2++) {
+                        // 같은 위치에 있는 격자는 겹치므로 건너뛰기
+                        if (r1 == r2 && (c1 <= c2 && c2 < c1 + 3 || c2 <= c1 && c1 < c2 + 3)) {
+                            continue;
+                        }
+                        
+                        // 두 번째 격자의 동전 개수 계산
+                        int coins2 = arr[r2][c2] + arr[r2][c2 + 1] + arr[r2][c2 + 2];
+                        
+                        // 최대값 갱신
+                        maxCoins = Math.max(maxCoins, coins1 + coins2);
                     }
-
-                    max = Math.max(max,count);
                 }
-
-                findCoin[i] = max;
-            }
-        }else{
-            for(int i = 0; i < n; i++){
-                int max = 0;
-                for(int j = 0; j < n - 5; j++){
-                    int count = 0;
-                    for(int k = 0; k < 3; k++){
-                        if(arr[i][j + k] == 1){count++;}
-                    }
-
-                    for(int l = 3; l < 6; l++){
-                        if(arr[i][j + l] == 1){count++;}
-                    }
-
-                    max = Math.max(max,count);
-                }
-
-                findCoin[i] = max;
             }
         }
-
-        Arrays.sort(findCoin);
-
-        if(n < 6){
-            System.out.println(findCoin[n-1] + findCoin[n -2]);
-        }else{
-            System.out.println(findCoin[n-1]);
-        }
+        
+        System.out.println(maxCoins);
     }
 }
