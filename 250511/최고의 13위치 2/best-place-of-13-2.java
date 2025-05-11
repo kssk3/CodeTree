bd3cd535af990fc578ba3753ee5754fa477b0b30
Mercury <kss8014@daum.net>
2025-05-11 20:36:49 +0900

    Title: 최고의 13위치 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250511/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n;
+
+    public static int[][] arr;
+
+    public static int[][] findCoin = new int[0][2];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        arr = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+
+        int[] findCoin = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int max = 0;
+            for(int j = 0; j < n - 2; j++){
+                int count = 0;
+                for(int k = 0; k < 3; k++){
+                    if(arr[i][j + k] == 1){count++;}
+                }
+
+                max = Math.max(max,count);
+            }
+
+            findCoin[i] = max;
+        }
+
+        Arrays.sort(findCoin);
+
+        System.out.println(findCoin[n-1] + findCoin[n-2]);
+    }
+}
