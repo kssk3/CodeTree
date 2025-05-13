d51ba524227375d3edc8c0f429543ad81bac5f89
Mercury <kss8014@daum.net>
2025-05-13 11:41:55 +0900

    Title: G or H 3, Time: 148ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250513/G or H 3/G-or-H-3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int MAX = 100;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 사람 수 
+        int n = sc.nextInt();
+        // 사진의 크기
+        int k = sc.nextInt();
+
+        int[] arr = new int[MAX];
+        char[] charArr = new char[MAX];
+
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            charArr[i] = sc.next().charAt(0);
+
+            max = Math.max(max, arr[i]);
+        }
+
+        int[] pos = new int[MAX];
+
+        for(int i = 0; i < n; i++){
+            if(charArr[i] == 'G'){
+                pos[arr[i]] = 1;
+            }else{
+                pos[arr[i]] = 2;
+            }
+        }
+
+
+        int maxCount = 0;
+        for(int i = 1; i <= max - k + 1; i++){
+            int point = 0;
+            for(int j = i; j <= i + k; j++){
+                point += pos[j];
+            }
+            maxCount = Math.max(maxCount, point);
+        }
+
+    System.out.println(maxCount);
+
+    }
+}
