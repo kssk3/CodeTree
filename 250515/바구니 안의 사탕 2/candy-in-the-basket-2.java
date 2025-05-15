0105e132f700f9f78934007a680e7578ed8a7955
Mercury <kss8014@daum.net>
2025-05-15 12:12:24 +0900

    Title: 바구니 안의 사탕 2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250515/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int MAX = 201;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[MAX];
+        int len = arr.length;
+
+        for(int i = 0; i < n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[b] = a;
+        }
+
+        int max = 0;
+        for(int i = 1; i < len; i++){
+            int a = Math.abs(i - k);
+            int b = Math.abs(i + k);
+            int sum = 0;
+            if(!isRange(a,b)){continue;}
+            for(int j = a; j <= b; j++){
+                sum += arr[j];
+            }
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+    }
+
+    private static boolean isRange(int n, int m){
+        return (1 <= n && n < 21 && 1 <= m && m < 21);
+    }
+}
