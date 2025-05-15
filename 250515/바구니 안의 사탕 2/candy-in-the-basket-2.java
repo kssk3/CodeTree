9b082471daf6e098d87c3fcb453e597d8f57b9cb
Mercury <kss8014@daum.net>
2025-05-15 12:43:15 +0900

    Title: 바구니 안의 사탕 2, Time: 159ms, Memory: 10MB, Status: Passed - Codetree

--- 250515/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 250515/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    public static final int MAX = 201;
+    public static final int MAX = 101;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -10,7 +10,6 @@ public class Main {
         int k = sc.nextInt();
 
         int[] arr = new int[MAX];
-        int len = arr.length;
 
         for(int i = 0; i < n; i++){
             int a = sc.nextInt();
@@ -19,22 +18,16 @@ public class Main {
         }
 
         int max = 0;
-        for(int i = 1; i < len; i++){
-            int a = Math.abs(i - k);
-            int b = Math.abs(i + k);
+        for(int i = 0; i <= 100; i++){
+            int a = Math.max(0, i - k);
+            int b = Math.min(100, i + k);
             int sum = 0;
-            if(!isRange(a,b)){continue;}
+
             for(int j = a; j <= b; j++){
                 sum += arr[j];
             }
             max = Math.max(max, sum);
         }
-
         System.out.println(max);
-
-    }
-
-    private static boolean isRange(int n, int m){
-        return (1 <= n && n < MAX && 1 <= m && m < MAX);
     }
 }
