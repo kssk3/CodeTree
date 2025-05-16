f2795bffb5107aabdfb46a00e3605f627ac207b4
Mercury <kss8014@daum.net>
2025-05-16 12:12:34 +0900

    Title: G or H 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250516/G or H 2/G-or-H-2.java
+++ 250516/G or H 2/G-or-H-2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    public static final int MAX = 21;
+    public static final int MAX = 101;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
