c9d87e539f8d11d521581b63b0412db398ecb59a
Mercury <kss8014@daum.net>
2025-05-20 10:57:05 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 150ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250520/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int cnt = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                for(int k = 1; k <= n; k++){
+                    if(i > a + 2 && j > b + 2 && k > c + 2){continue;}
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
