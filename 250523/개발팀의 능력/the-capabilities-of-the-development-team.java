8191a77a36ece629731238db003431754eb5bb63
Mercury <kss8014@daum.net>
2025-05-23 11:57:15 +0900

    Title: 개발팀의 능력, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250523/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int MAX = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+
+        for(int i = 0; i < 5; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int min = MAX;
+        for(int a = 0; a < 5; a++){
+            for(int b = a + 1; b < 5; b++){
+                for(int c = 0; c < 5; c++){
+                    for(int d = c + 1; d < 5; d++){
+                        if(a == c || a == d || b == c || b == d){continue;}
+
+                        int e = 0;
+                        for(int i = 0; i < 5; i++){
+                            if(i == a || i == b || i == c || i == d){continue;}
+                            e = i;
+                        }
+
+                        int sum1 = arr[a] + arr[b];
+                        int sum2 = arr[c] + arr[b];
+                        int maxSum = Math.max(Math.max(sum1, sum2), e);
+                        int minSum = Math.min(Math.min(sum1, sum2), e);
+                        min = Math.min(min, maxSum - minSum);
+                    }
+                }
+            }
+        }
+        if(min == 0){
+            System.out.println(-1);
+        }else{
+            System.out.println(min);
+        }
+    }
+}
