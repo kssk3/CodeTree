d41241ecfbbd4db529cc15c51cd15fee33edf700
Mercury <kss8014@daum.net>
2025-06-11 14:42:51 +0900

    Title: 등차수열, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 250611/등차수열/arithmetic-sequence.java
+++ 250611/등차수열/arithmetic-sequence.java
@@ -2,37 +2,30 @@ import java.util.*;
 
 public class Main {
 
-    public static final int MAX = 101;
-    public static int max;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[] arr = new int[MAX];
+        int[] arr = new int[n];
         
-        max = 0;
-        for(int i = 1; i <= n; i++){
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
-            max = Math.max(max, arr[i]);
         }
 
-        int total = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = i + 1; j <= n; j++){
-                total += found(arr[i], arr[j]);
+        int maxCount = 0;
+
+        for(int k = 1; k <= 200; k++){
+            int cnt = 0;
+
+            for(int i = 0; i < n; i++){
+                for(int j = i + 1; j < n; j++){
+                    if(k * 2 == arr[i] + arr[j])cnt++;
+                }
             }
-        }
-        System.out.println(total);
-    }
 
-    private static int found(int a, int b){
-        int cnt = 0;
-        for(int i = 1; i <= max; i++){
-            int x = Math.abs(a - i);
-            int y = Math.abs(b - i);
-            if(x == y)cnt++;
+            maxCount = Math.max(maxCount, cnt);
         }
-        return cnt;
+
+        System.out.println(maxCount);
     }
 }
