59309d35260095c7e59b212b2cfb374dff2d6ce4
Mercury <kss8014@daum.net>
2025-06-18 12:04:24 +0900

    Title: 3개의 선 2, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

--- 250618/3개의 선 2/three-lines-2.java
+++ 250618/3개의 선 2/three-lines-2.java
@@ -12,38 +12,45 @@ public class Main {
         int[] dy = new int[n];
 
         for(int i = 0; i < n; i++){
-            dx[i] = sc.nextInt();
-            dy[i] = sc.nextInt();
+            dx[i] = sc.nextInt(); // {1, 0, 1, 2, 1, 3}
+            dy[i] = sc.nextInt(); // {7, 0, 2, 0, 4, 4}
         }
 
         int ans = 0;
         for(int i = 0; i < MAX; i++){
             for(int j = 0; j < MAX; j++){
                 for(int k = 0; k < MAX; k++){
-                    boolean found = false;
+                    boolean found = true;
 
                     for(int a = 0; a < n; a++){
-                        if(dx[a] == i && dx[a] == j && dx[a] == k){
-                            found = true;
-                            break;
-                        }
+                        if(dx[a] == i || dx[a] == j || dx[a] == k)continue;
 
-                        if(dx[a] == i && dx[a] == j && dy[a] == k){
-                            found = true;
-                            break;
-                        }
-
-                        if(dx[a] == i && dy[a] == j && dy[a] == k){
-                            found = true;
-                            break;
-                        }
-
-                        if(dy[a] == i && dy[a] == j && dy[a] == k){
-                            found = true;
-                            break;
-                        }
+                        found = false;
                     }
+                    if(found)ans = 1;
+                    
+                    found = true;
+                    for(int a = 0; a < n; a++){                        
+                        if(dx[a] == i || dx[a] == j || dy[a] == k)continue;
 
+                        found = false;
+                    }
+                    if(found)ans = 1;
+
+                    found = true;
+                    for(int a = 0; a < n; a++){
+                        if(dx[a] == i || dy[a] == j || dy[a] == k)continue;
+
+                        found = false;
+                    }
+                    if(found)ans = 1;
+
+                    found = true;
+                    for(int a = 0; a < n; a++){
+                        if(dy[a] == i || dy[a] == j || dy[a] == k)continue;
+
+                        found = false;
+                    }
                     if(found)ans = 1;
                 }
             }
