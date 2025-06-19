a2d5a1a3b67cb78d04fb4494f40003f520ff098c
Mercury <kss8014@daum.net>
2025-06-19 12:50:11 +0900

    Title: 팀으로 하는 틱택토 2, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

--- 250619/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
+++ 250619/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.java
@@ -8,40 +8,61 @@ public class Main {
 
         for(int i = 0; i < 3; i++){
             String s = Integer.toString(sc.nextInt());
-            String[] srr = s.split("");
             for(int j = 0; j < 3; j++){
-                board[i][j] = Integer.parseInt(srr[j]);
+                board[i][j] = s.charAt(j) - '0';
             }
         }
 
+        boolean[][] teams = new boolean[10][10];
+        
+        for(int i = 0; i < 3; i++){
+            addTeam(board[i][0], board[i][1], board[i][2], teams);
+        }
+
+        for(int i = 0; i < 3; i++){
+            addTeam(board[0][i], board[1][i], board[2][i], teams);
+        }
+
+        addTeam(board[0][0], board[1][1], board[2][2], teams);
+        addTeam(board[0][2], board[1][1], board[2][0], teams);
+
         int cnt = 0;
-        for(int i = 0; i < 3; i++){
-            if(isWingLine(board[i][0], board[i][1], board[i][2]))cnt++;
+        for(int i = 1; i <= 9; i++){
+            for(int j = i + 1; j <= 9; j++){
+                if(teams[i][j]){
+                    cnt++;
+                }
+            }
         }
-
-        for(int i = 0; i < 3; i++){
-            if(isWingLine(board[0][i], board[1][i], board[2][i]))cnt++;
-        }
-
-        if(isWingLine(board[0][0], board[1][1], board[2][2]))cnt++;
-
-        if(isWingLine(board[0][2], board[1][1], board[2][0]))cnt++;
 
         System.out.println(cnt);
     }
 
-    private static boolean isWingLine(int a, int b, int c){
+    private static void addTeam(int a, int b, int c, boolean[][] teams){
         int[] count = new int[10];
 
         count[a]++;
         count[b]++;
         count[c]++;
         
-        for(int i = 0; i < 10; i++){
-            if(count[i] == 2){
-                return true;
+        int uniqueCount = 0;
+        int first = -1, second = -1;
+        
+        for(int i = 1; i <= 9; i++){
+            if(count[i] > 0){
+                uniqueCount++;
+                if(first == -1) first = i;
+                else if(second == -1) second = i;
             }
         }
-        return false;
+        
+        if(uniqueCount == 2){
+            if(first > second){
+                int temp = first;
+                first = second;
+                second = temp;
+            }
+            teams[first][second] = true;
+        }
     }
 }
