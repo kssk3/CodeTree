9f3e5b8b0feb95c4bc4a1fed20c59eb4153ad309
Mercury <kss8014@daum.net>
2025-07-19 15:42:37 +0900

    Title: 언덕 깎기, Time: 169ms, Memory: 13MB, Status: Passed - Codetree

--- 250719/언덕 깎기/hill-cutting.java
+++ 250719/언덕 깎기/hill-cutting.java
@@ -1,11 +1,10 @@
 import java.util.*;
 
 public class Main {
+    
+    public static final int MAX = 100;
 
     public static final int HEIGHT = 17;
-
-    public static int max = Integer.MIN_VALUE;
-    public static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,27 +15,26 @@ public class Main {
 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
-
-            max = Math.max(max, arr[i]);
-            min = Math.min(min, arr[i]);
         }
 
-        int plusCnt = 0;
-        int minCnt = 0;
+        int min = Integer.MAX_VALUE;
+        for(int low = 0; low <= MAX - HEIGHT; low++){
+            int high = low + HEIGHT;
+            int cost = 0;
 
-        for(int i = 1; i < 100; i++){
-            if(max - min == HEIGHT){
-                break;
+            for(int j = 0; j < n; j++){
+                if(arr[j] < low){
+                    int diff = low - arr[j];
+                    cost += diff * diff;
+                }else if(high < arr[j]){
+                    int diff = arr[j] - high;
+                    cost += diff * diff;
+                }
             }
 
-            if(max - min != HEIGHT){
-                max -= 1;
-                min += 1;
-                plusCnt++;
-                minCnt++;
-            }
+            min = Math.min(min, cost);
         }
 
-        System.out.println((plusCnt * plusCnt) + (minCnt * minCnt));
+        System.out.println(min);
     }
 }
