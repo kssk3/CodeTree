2660fa6dee1539059af6901369a5fba23630ff78
Mercury <kss8014@daum.net>
2025-07-28 15:27:25 +0900

    Title: 두 직사각형, Time: 110ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250728/두 직사각형/two-rectangles.java
+++ 250728/두 직사각형/two-rectangles.java
@@ -6,40 +6,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        found(sc);
+        solution(sc);
     }
 
-    public static void found(Scanner sc){
-        if(foundX(sc) && foundY(sc)){
+    public static void solution(Scanner sc){
+        if(isFound(sc)){
             System.out.println("overlapping");
         }else{
             System.out.println("nonoverlapping");
         }
     }
 
-    private static boolean foundX(Scanner sc){
+    private static boolean isFound(Scanner sc){
         x1 = sc.nextInt();
-        x2 = sc.nextInt();
-        a1 = sc.nextInt();
-        a2 = sc.nextInt();
-
-        if(a1 < x2 || a2 < x1){
-            return false;
-        }else{
-            return true;
-        }
-    }
-
-    private static boolean foundY(Scanner sc){
         y1 = sc.nextInt();
+        x2 = sc.nextInt();
         y2 = sc.nextInt();
+
+        a1 = sc.nextInt();
         b1 = sc.nextInt();
+        a2 = sc.nextInt();
         b2 = sc.nextInt();
 
-        if(b1 < y2 || b2 < y1){
-            return false;
-        }else{
+        if(((a1 < x2) && (b1 < y2)) || ((x1 < a1) && (y1 < b2))){
             return true;
+        }else{
+            return false;
         }
     }
 }
