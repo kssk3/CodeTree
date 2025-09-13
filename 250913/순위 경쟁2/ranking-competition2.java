7d852225b0d0a94155c1d5407c621af3ecd90cb9
Mercury <kss8014@daum.net>
2025-09-13 14:42:07 +0900

    Title: 순위 경쟁2, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250913/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int cntA = 0;
+        int cntB = 0;
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            char target = sc.next().charAt(0);
+            int num = sc.nextInt();
+
+            // System.out.print("ans = " + ans);
+            int tmp = 0;
+            if(target == 'A'){
+                tmp = cntA;
+                cntA += num;
+                }
+
+            if(target == 'B'){
+                tmp = cntB;
+                cntB += num;
+            }
+
+            // System.out.println(" target = " + target + ", num = " + num + ", cntA = " + cntA + ", cntB = " + cntB);
+            if(target == 'A' && tmp > cntB && num > 0)continue;
+            if(target == 'A' && tmp <= cntB && num > 0 && cntA >= cntB)ans++;
+            if(target == 'A' && tmp >= cntB && num < 0 && cntA < cntB)ans++;
+
+            if(target == 'B' && tmp > cntA && num > 0)continue;
+            if(target == 'B' && tmp <= cntA && num > 0 && cntB >= cntA)ans++;
+            if(target == 'B' && tmp >= cntA && num < 0 && cntA > cntB)ans++;
+
+            // if(target == 'A' && tmp < cntB && cntA > cntB)ans++;// target = 'A', num = +1, tmp == cntB, cntA = 1, cntB = 0, ans++;
+            // if(target == 'A' && tmp > cntB && num > 0)continue;
+            // if(target == 'A' && tmp <= cntB && num > 0 && cntA => cntB)ans++;
+            // if(target == 'A' && tmp => cntB && num < 0 && cntA < cntB)ans++;
+
+            // if(target == 'B' && tmp < cntA && cntA =< cntB)ans++;// target = 'B', num = +1, tmp = -1, cntA= 0, cntB = 0, ans++;
+            // if(target == 'B' && tmp > cntA && num > 0)continue; // tmp = 1, cntA = -1, cntB = 1
+            // if(target == 'B' && tmp <= cntA && num > 0 && cntB = cntA)ans++;
+            // if(target == 'B' && tmp => cntA && num < 0 && cntA > cntB)ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
