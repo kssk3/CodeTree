108a728f7ba3b816b945d5d89056db2bb78fc8f5
Mercury <kss8014@daum.net>
2025-09-18 16:04:49 +0900

    Title: L, R 그리고 B, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

--- 250918/L, R 그리고 B/l-r-and-b.java
+++ 250918/L, R 그리고 B/l-r-and-b.java
@@ -4,19 +4,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         char[][] board = new char[10][10];
+        
         int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
         for(int i = 0; i < 10; i++){
             String line = sc.next();
             for(int j = 0; j < 10; j++){
                 board[i][j] = line.charAt(j);
-                if(board[i][j] == 'B'){
-                    x1 = i; y1 = j;
-                }
-                if(board[i][j] == 'L'){
-                    x2 = i; y2 = j;
-                }
+                if(board[i][j] == 'B'){x1 = i; y1 = j;}
+                if(board[i][j] == 'L'){x2 = i; y2 = j;}
             }
         }
-        System.out.println(Math.abs(x2 - x1) + Math.abs(y2 - y1) - 1);
+
+        int dist = 0;
+        boolean found = false;
+
+        if(x1 == x2){
+            int min = Math.min(y1, y2), max = Math.max(y1, y2);
+            for(int i = min + 1; i < max; i++){
+                if(board[x1][i] == 'R')found = true;
+            }
+            dist = Math.abs(y1 - y2) - 1;
+        }else if(y1 == y2){
+            int min = Math.min(x1, x2), max = Math.max(x1, x2);
+            for(int i = min + 1; i < max; i++){
+                if(board[i][y1] == 'R')found = true;
+            }
+            dist = Math.abs(x1 - x2) - 1;
+        }else{
+            dist = Math.abs(x1 - x2) + Math.abs(y1 - y2) - 1;
+        }
+
+        System.out.println(found ? dist + 2 : dist);
     }
 }
